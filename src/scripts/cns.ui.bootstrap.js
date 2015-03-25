966edfae72663f674947ccf356fa508f655bf503
s-aleh <aleh.stryk@gmail.com>
2015-03-24 23:12:08 -0500

    Commit

--- src/scripts/cns.ui.bootstrap.js
+++ src/scripts/cns.ui.bootstrap.js
@@ -1,9 +1,77 @@
 'use strict';
 
-angular.module('cns.ui.bootstrap', ['cns.ui.templates', 'cns.ui.bootstrap.pagination', 'cns.ui.bootstrap.pager', 'cns.ui.bootstrap.rating'])
+angular.module('cns.ui.bootstrap', ['cns.ui.bootstrap.templates', 'cns.ui.bootstrap.pagination', 'cns.ui.bootstrap.pager', 'cns.ui.bootstrap.rating'])
     .constant('MODULE_VERSION', '0.0.1');
 
-angular.module('cns.ui.templates', ['templates/directives/rating.html', 'templates/directives/pager.html', 'templates/directives/pagination.html']);
+angular.module('cns.ui.bootstrap.templates', ['../src/templates/directives/pager.html', '../src/templates/directives/pagination.html', '../src/templates/directives/rating.html']);
+
+
+
+angular.module('cns.ui.bootstrap.pager', [])
+    .constant('cfgPager', {
+        curPage: 1,
+        text: {
+            prev: 'Previous',
+            next: 'Next'
+        },
+        icon: {
+            prev: '\u2190',
+            next: '\u2192'
+        }
+    })
+    .controller('PagerController', ['$scope', '$attrs', 'cfgPager', function($scope, $attrs, cfgPager) {
+        this.init = function() {
+            $scope.aligned = angular.isDefined($attrs.aligned) ? true : false;
+            $scope.rotate = angular.isDefined($attrs.rotate) ? true : false;
+            $scope.noIcon = angular.isDefined($attrs.noIcon) ? false : true;
+            $scope.text = {};
+            $scope.text.prev = angular.isDefined($attrs.prevText) ? $attrs.prevText : cfgPager.text.prev;
+            $scope.text.next = angular.isDefined($attrs.nextText) ? $attrs.nextText : cfgPager.text.next;
+            $scope.icon = {};
+            $scope.icon.prev = angular.isDefined($attrs.prevIcon) ? $attrs.prevIcon : cfgPager.icon.prev;
+            $scope.icon.next = angular.isDefined($attrs.nextIcon) ? $attrs.nextIcon : cfgPager.icon.next;
+        };
+        $scope.setCurPage = function(event, page) {
+            event.preventDefault();
+            switch(page) {
+                case 'prev':
+                    $scope.ngModel = ($scope.ngModel > 1) ? $scope.ngModel - 1 :
+                        $scope.rotate ? $scope.totalPages : $scope.ngModel;
+                    break;
+                case 'next':
+                    $scope.ngModel = ($scope.ngModel < $scope.totalPages) ? $scope.ngModel + 1 :
+                        $scope.rotate ? 1 : $scope.ngModel;
+                    break;
+            }
+        };
+        $scope.$watch("totalPages", function() {
+            $scope.ngModel = ($scope.totalPages < $scope.ngModel) ? $scope.totalPages : ($scope.ngModel == 0) ? 1 : $scope.ngModel;
+        });
+    }])
+    .directive('cnsPager', [function(){
+        return {
+            controller: 'PagerController',
+            link: function(scope, element, attributes, controllers) {
+                var pagerCtrl = controllers[0],
+                    ngModelCtrl = controllers[1];
+                if(!pagerCtrl) {
+                    console.log('Pager directive error');
+                    return;
+                }
+                pagerCtrl.init();
+            },
+            restrict: 'E',
+            require: ['cnsPager', 'ngModel'],
+            scope: {
+                ngModel: '=',
+                totalPages: '@',
+                textButton: '=',
+                textIcon: '='
+            },
+            templateUrl: '../src/templates/directives/pager.html'
+        };
+    }]);
+
 
 angular.module('cns.ui.bootstrap.pagination', [])
     .constant('cfgPagination', {
@@ -94,75 +162,9 @@ angular.module('cns.ui.bootstrap.pagination', [])
                 ngModel: '=',
                 totalPages: '@'
             },
-            templateUrl: 'templates/directives/pagination.html'
+            templateUrl: '../src/templates/directives/pagination.html'
         };
     }]);
-
-angular.module('cns.ui.bootstrap.pager', [])
-    .constant('cfgPager', {
-        curPage: 1,
-        text: {
-            prev: 'Previous',
-            next: 'Next'
-        },
-        icon: {
-            prev: '\u2190',
-            next: '\u2192'
-        }
-    })
-    .controller('PagerController', ['$scope', '$attrs', 'cfgPager', function($scope, $attrs, cfgPager) {
-        this.init = function() {
-            $scope.aligned = angular.isDefined($attrs.aligned) ? true : false;
-            $scope.rotate = angular.isDefined($attrs.rotate) ? true : false;
-            $scope.noIcon = angular.isDefined($attrs.noIcon) ? false : true;
-            $scope.text = {};
-            $scope.text.prev = angular.isDefined($attrs.prevText) ? $attrs.prevText : cfgPager.text.prev;
-            $scope.text.next = angular.isDefined($attrs.nextText) ? $attrs.nextText : cfgPager.text.next;
-            $scope.icon = {};
-            $scope.icon.prev = angular.isDefined($attrs.prevIcon) ? $attrs.prevIcon : cfgPager.icon.prev;
-            $scope.icon.next = angular.isDefined($attrs.nextIcon) ? $attrs.nextIcon : cfgPager.icon.next;
-        };
-        $scope.setCurPage = function(event, page) {
-            event.preventDefault();
-            switch(page) {
-                case 'prev':
-                    $scope.ngModel = ($scope.ngModel > 1) ? $scope.ngModel - 1 :
-                        $scope.rotate ? $scope.totalPages : $scope.ngModel;
-                    break;
-                case 'next':
-                    $scope.ngModel = ($scope.ngModel < $scope.totalPages) ? $scope.ngModel + 1 :
-                        $scope.rotate ? 1 : $scope.ngModel;
-                    break;
-            }
-        };
-        $scope.$watch("totalPages", function() {
-            $scope.ngModel = ($scope.totalPages < $scope.ngModel) ? $scope.totalPages : ($scope.ngModel == 0) ? 1 : $scope.ngModel;
-        });
-    }])
-    .directive('cnsPager', [function(){
-        return {
-            controller: 'PagerController',
-            link: function(scope, element, attributes, controllers) {
-                var pagerCtrl = controllers[0],
-                    ngModelCtrl = controllers[1];
-                if(!pagerCtrl) {
-                    console.log('Pager directive error');
-                    return;
-                }
-                pagerCtrl.init();
-            },
-            restrict: 'E',
-            require: ['cnsPager', 'ngModel'],
-            scope: {
-                ngModel: '=',
-                totalPages: '@',
-                textButton: '=',
-                textIcon: '='
-            },
-            templateUrl: 'templates/directives/pager.html'
-        };
-    }]);
-
 angular.module('cns.ui.bootstrap.rating', [])
     .constant('ratingCfg', {
         maxRating: 5,
@@ -212,49 +214,50 @@ angular.module('cns.ui.bootstrap.rating', [])
                 ngModel: '=',
                 rating: '@'
             },
-            templateUrl: 'templates/directives/rating.html',
+            templateUrl: '../src/templates/directives/rating.html',
             transclude: true
         };
     }]);
 
-angular.module("templates/directives/pagination.html", []).run(["$templateCache", function($templateCache) {
-    $templateCache.put("templates/directives/pagination.html",
-    "<nav>" +
-    "   <ul class=\"pagination\" ng-class=\"bootstrapClass\">" +
-    "       <li ng-if=\"show.first\" ng-class=\"{'disabled': ngModel == 1}\" ng-click=\"setCurPage($event, 'first')\"><a href=\"#\">{{text.first}}</a></li>" +
-    "       <li ng-if=\"show.prev\" ng-class=\"{'disabled': ngModel == 1}\" ng-click=\"setCurPage($event, 'prev')\"><a href=\"#\">{{text.prev}}</a></li>" +
-    "       <li ng-if=\"$index + 1 >= leftPages && $index + 1 <= rightPages\" ng-class=\"{'active': $index + 1 == ngModel}\" ng-click=\"setCurPage($event, $index + 1)\"" +
-    "           ng-repeat=\"page in pages track by $index | limitTo: 10\"><a href=\"#\">{{$index + 1}}</a></li>" +
-    "       <li ng-if=\"show.next\" ng-class=\"{'disabled': ngModel == totalPages}\" ng-click=\"setCurPage($event, 'next')\"><a href=\"#\">{{text.next}}</a></li>" +
-    "       <li ng-if=\"show.last\" ng-class=\"{'disabled': ngModel == totalPages}\" ng-click=\"setCurPage($event, 'last')\"><a href=\"#\">{{text.last}}</a></li>" +
-    "   </ul>" +
-    "</nav>"
+
+angular.module("../src/templates/directives/pager.html", []).run(["$templateCache", function($templateCache) {
+$templateCache.put("../src/templates/directives/pager.html",
+	"<nav>" +
+	"  <ul class=\"pager\">" +
+	"    <li ng-class=\"{'disabled': ngModel == 1 && !rotate, 'previous': aligned}\" ng-click=\"setCurPage($event, 'prev')\">" +
+	"        <a href=\"#\"><span ng-if=\"noIcon\" aria-hidden=\"true\">{{icon.prev}}</span> {{text.prev}}</a>" +
+	"    </li>" +
+	"    <li ng-class=\"{'disabled': ngModel == totalPages && !rotate, 'next': aligned}\" ng-click=\"setCurPage($event, 'next')\">" +
+	"        <a href=\"#\">{{text.next}} <span ng-if=\"noIcon\" aria-hidden=\"true\">{{icon.next}}</span></a>" +
+	"    </li>" +
+	"  </ul>" +
+	"</nav>"
 );
 }]);
 
-angular.module("templates/directives/pager.html", []).run(["$templateCache", function($templateCache) {
-    $templateCache.put("templates/directives/pager.html",
-    "<nav>" +
-    "   <ul class=\"pager\">" +
-    "       <li ng-class=\"{'disabled': ngModel == 1 && !rotate, 'previous': aligned}\" ng-click=\"setCurPage($event, 'prev')\">" +
-    "           <a href=\"#\"><span ng-if=\"noIcon\" aria-hidden=\"true\">{{icon.prev}}</span> {{text.prev}}</a>" +
-    "       </li>" +
-    "       <li ng-class=\"{'disabled': ngModel == totalPages && !rotate, 'next': aligned}\" ng-click=\"setCurPage($event, 'next')\">" +
-    "           <a href=\"#\">{{text.next}} <span ng-if=\"noIcon\" aria-hidden=\"true\">{{icon.next}}</span></a>" +
-    "       </li>" +
-    "   </ul>" +
-    "</nav>"
-    );
+angular.module("../src/templates/directives/pagination.html", []).run(["$templateCache", function($templateCache) {
+$templateCache.put("../src/templates/directives/pagination.html",
+	"<nav>" +
+	"    <ul class=\"pagination\" ng-class=\"bootstrapClass\">" +
+	"        <li ng-if=\"show.first\" ng-class=\"{'disabled': ngModel == 1}\" ng-click=\"setCurPage($event, 'first')\"><a href=\"#\">{{text.first}}</a></li>" +
+	"        <li ng-if=\"show.prev\" ng-class=\"{'disabled': ngModel == 1}\" ng-click=\"setCurPage($event, 'prev')\"><a href=\"#\">{{text.prev}}</a></li>" +
+	"        <li ng-if=\"$index + 1 >= leftPages && $index + 1 <= rightPages\" ng-class=\"{'active': $index + 1 == ngModel}\" ng-click=\"setCurPage($event, $index + 1)\"" +
+	"            ng-repeat=\"page in pages track by $index | limitTo: 10\"><a href=\"#\">{{$index + 1}}</a></li>" +
+	"        <li ng-if=\"show.next\" ng-class=\"{'disabled': ngModel == totalPages}\" ng-click=\"setCurPage($event, 'next')\"><a href=\"#\">{{text.next}}</a></li>" +
+	"        <li ng-if=\"show.last\" ng-class=\"{'disabled': ngModel == totalPages}\" ng-click=\"setCurPage($event, 'last')\"><a href=\"#\">{{text.last}}</a></li>" +
+	"    </ul>" +
+	"</nav>"
+);
 }]);
 
-angular.module("templates/directives/rating.html", []).run(["$templateCache", function($templateCache) {
-    $templateCache.put("templates/directives/rating.html",
-    "<span ng-mouseleave=\"mouseleave($index)\" ng-keydown=\"keydown($event)\">" +
-    "   <span ng-transclude></span>" +
-    "   <span href=\"#\" ng-repeat=\"value in values track by $index\" ng-mouseover=\"mouseenter($index)\" ng-click=\"setRating($index)\">" +
-    "       <span ng-if=\"$index + 1 <= r\" class=\"glyphicon\" ng-class=\"iconValued\" aria-hidden=\"true\"></span>" +
-    "       <span ng-if=\"$index + 1 > r\" class=\"glyphicon\" ng-class=\"iconUnvalued\" aria-hidden=\"true\"></span>" +
-    "   </span>" +
-    "</span>"
-    );
+angular.module("../src/templates/directives/rating.html", []).run(["$templateCache", function($templateCache) {
+$templateCache.put("../src/templates/directives/rating.html",
+	"<span ng-mouseleave=\"mouseleave($index)\" ng-keydown=\"keydown($event)\">" +
+	"    <span ng-transclude></span>" +
+	"    <span href=\"#\" ng-repeat=\"value in values track by $index\" ng-mouseover=\"mouseenter($index)\" ng-click=\"setRating($index)\">" +
+	"        <span ng-if=\"$index + 1 <= r\" class=\"glyphicon\" ng-class=\"iconValued\" aria-hidden=\"true\"></span>" +
+	"        <span ng-if=\"$index + 1 > r\" class=\"glyphicon\" ng-class=\"iconUnvalued\" aria-hidden=\"true\"></span>" +
+	"    </span>" +
+	"</span>"
+);
 }]);
